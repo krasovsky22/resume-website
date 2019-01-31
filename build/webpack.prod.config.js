e23d3e2838238d6c97c9585f22cce1efa71daa39
ukrasouski <ukrasouski@ambrygen.com>
2019-01-31 11:17:16 -0800

    added express server

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -1,10 +1,13 @@
-const merge = require('webpack-merge');
-const config = require('./webpack.config');
+const merge = require("webpack-merge");
+const { resolve } = require("./utils");
+const config = require("./webpack.config");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = merge(config, {
-  new CleanWebpackPlugin(["dist"], {
-    root: resolve("./")
-  }),
-  mode: 'production'
+  mode: "production",
+  plugins: [
+    new CleanWebpackPlugin(["dist"], {
+      root: resolve("./")
+    })
+  ]
 });
