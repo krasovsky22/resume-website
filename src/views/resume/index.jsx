11c840168322ab2c751afb38ef4dc83e612315ca
ukrasouski <ukrasouski@ambrygen.com>
2019-02-01 17:29:45 -0800

    `

--- src/views/resume/index.jsx
+++ src/views/resume/index.jsx
@@ -2,11 +2,27 @@
 import React, { Component } from "react";
 import { Container, Row, Col } from "reactstrap";
 
-import { default as NameRow } from "./rows/Name";
-
 class ResumeIndex extends Component {
   render() {
-    return <>Resume Here</>;
+    return (
+      <Row>
+        <Col md={12}>
+          <section className="about-me line padding_30 padbot_45">
+            <div className="section-title">
+              <span />
+              <h2>Resume</h2>
+            </div>
+            <Row>
+              <Col md={6} className="working-history padding_15 padbot_30">
+                <ul className="timeline">
+                  <li />
+                </ul>
+              </Col>
+            </Row>
+          </section>
+        </Col>
+      </Row>
+    );
   }
 }
 
--- src/views/resume/rows/Name.jsx
+++
@@ -1,8 +0,0 @@
-import React, { memo } from "react";
-import { Row, Col } from "reactstrap";
-
-export default memo(props => (
-  <Row>
-    <Col xs={3}> Name 123</Col>
-  </Row>
-));
